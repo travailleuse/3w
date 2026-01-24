54289a88887d6e2fdeb3e9b0c6341103fa36658d
travailleuse <1610504934@qq.com>
2026-01-24 18:39:25 +0800

    优化了./dataStore/indexedDbUtils.js的结构，加油吧！！！
    Signed-off-by: travailleuse <1610504934@qq.com>

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# 3w
+# 介绍
+
+## 文件目录机构
+
+刚刚写这个项目，`indexedDB`是Web的数据库，我模仿`mangoDb`的写法实现了些许功能。
+
+```shell
+/dataStore
+```
+
--- dataStore/indexedDbUtils.js
+++ dataStore/indexedDbUtils.js
@@ -7,7 +7,58 @@
  * @copyright Copyright (c) 2026 travailleuse
  */
 
+
+/**
+ * @class StoreConfig
+ * @description storeConfig for create or delete object store
+ */
+class StoreConfig {
+    /**
+     * @type {Map<string, IDBObjectStoreParameters>}
+     * @description createdObjectStore
+     * @property {string} name
+     * @property {IDBObjectStoreParameters} options
+     */
+    #createdObjectStore = new Map();
+
+    /**
+     * @type {Array<string>}
+     */
+    #deletedObjectStoreNames = [];
+
+    addObjectStore(name, options) {
+        this.#createdObjectStore.set(name, options);
+        return this;
+    }
+
+    removeAddedObjectStore(name) {
+        this.#createdObjectStore.delete(name);
+        return this;
+    }
+
+    deleteONeObjectStore(name) {
+        this.#deletedObjectStoreNames.push(name);
+        return this;
+    }
+
+    getStoreConfig() {
+        return {
+            createdObjectStore: this.#createdObjectStore,
+            deletedObjectStoreNames: this.#deletedObjectStoreNames
+        }
+    }
+}
+
 class IndexDbManager {
+    constructor() {
+        throw new Error('IndexDbManager is a static class');
+    }
+
+    /**
+     * @type {Map<string, IDBDatabase>}
+     * @private
+     * @description name2db
+     */
     static #name2db = new Map();
 
     /**
@@ -15,37 +66,71 @@ class IndexDbManager {
      * 
      * @returns {Promise<Array<T>>}
      */
-    static showDb() {
-        return indexedDB.databases();
+    static async showDbs() {
+        return await indexedDB.databases();
     }
+
+    /**
+     * 
+     * @param {string} name 
+     * @returns {Promise<void>}
+     */
+    static async dropDb(name) {
+        return indexedDB.deleteDatabase(name);
+    }
+
+    /**
+     * 
+     * @param {string} name 
+     * @returns {}
+     */
+    static async getDb(name) {
+        return this.#name2db.get(name);
+    }
+
     /**
      * 
      * @param {string} name 
      * @param {null} version 
-     * @param {*} config 
+     * @param {StoreConfig} storeConfig 
      * @returns {Promise<IDBDatabase>}
      */
-    static async createDbIntance(name, version, config) {
+    static async use(name, version, storeConfig) {
+        /**
+         * @type {IDBDatabase}
+         */
         let db = null;
-        const getDb = () => {
-            const req = indexedDB.open(name, version);
-            return new Promise((resolve, reject) => {
-                req.onsuccess = e => {
-                    this.#name2db.set(name, e.target.result);
-                    db = e.target.result;
-                    resolve(db);
-                };
 
-                req.onupgradeneeded = e => {
-                    db = e.target.result;
-                    config.forEach(ele => {
-                        db.createObjectStore(ele, { keyPath: 'id', autoIncrement: true });
-                    })
-                }
-
-                req.onerror = e => reject(e.target.error);
-            })
+        /**
+         * first check if db exist, by name and version. if yes, return it.
+         */
+        if(this.#name2db.has(name)) {
+            db = this.#name2db.get(name);
+            if (currentDb.version === version) {
+                return Promise.resolve(currentDb);
+            }
         }
-        return getDb();
+
+        const req = indexedDB.open(name, version);
+        return new Promise((resolve, reject) => {
+            req.onsuccess = e => {
+                this.#name2db.set(name, e.target.result);
+                db = e.target.result;
+                resolve(db);
+            };
+
+            req.onupgradeneeded = e => {
+                db = e.target.result;
+                storeConfig.getStoreConfig().createdObjectStore.forEach((options, name) => {
+                    const store = db.createObjectStore(name, options);
+                });
+
+                storeConfig.getStoreConfig().deletedObjectStoreNames.forEach(name => {
+                    db.deleteObjectStore(name);
+                });
+            }
+
+            req.onerror = e => reject(e.target.error);
+        })
     }
 }

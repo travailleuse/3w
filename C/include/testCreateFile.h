bcc6b1f529f25a2298cc4470b64c2d13cf4cea97
travailleuse <1610504934@qq.com>
2026-02-06 03:06:42 +0800

    finishing file create multithreading
    Signed-off-by: travailleuse <1610504934@qq.com>

---
+++ C/include/testCreateFile.h
@@ -0,0 +1,15 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void createFile();
+
+void closeAllFile();
+
+void generateRandomInt();
+
+void readFiles();
+
+void removeAllFiles();
+
+void testAll();
